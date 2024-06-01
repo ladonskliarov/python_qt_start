688f697faa3ba6520bf321867d04464331864b6c
Ladon Skliarov <80324267+ladonskliarov@users.noreply.github.com>
2024-06-01 17:09:43 +0300

    Update py_qt_start.py

--- py_qt_start.py
+++ py_qt_start.py
@@ -1,16 +1,15 @@
 import json
-from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QListWidget, QLineEdit, QTextEdit, QInputDialog, \
-    QHBoxLayout, QVBoxLayout, QFormLayout
+    QHBoxLayout, QVBoxLayout
 
 app = QApplication([])
 notes = []
 
 '''Інтерфейс'''
 # Параметри вікна
-notes_win = QWidget()
-notes_win.setWindowTitle('Розумні нотатки')
-notes_win.resize(900, 600)
+window = QWidget()
+window.setWindowTitle('Розумні нотатки')
+window.resize(900, 600)
 
 # Віджети
 list_notes = QListWidget()
@@ -59,7 +58,85 @@ col_2.addLayout(row_4)
 
 layout_notes.addLayout(col_1, stretch=2)
 layout_notes.addLayout(col_2, stretch=1)
-notes_win.setLayout(layout_notes)
+window.setLayout(layout_notes)
+
+'''Функціональність'''
+# Зберегти нотатки у json
+def save_notes_to_json():
+    with open("notes_data.json", "w") as file:
+        json.dump(notes, file, indent=4, ensure_ascii=False, sort_keys=True)
+
+# Завантажити нотатки з json
+def load_notes_from_json():
+    try:
+        with open("notes_data.json", "r") as file:
+            notes.extend(json.load(file))
+    except FileNotFoundError:
+        pass
+
+def show_note():
+    key = list_notes.selectedItems()[0].text()
+    for note in notes:
+        if note['name'] == key:
+            field_text.setText(note['content'])
+            list_tags.clear()
+            list_tags.addItems(note['tags'])
+
+def add_note():
+    note_name, ok = QInputDialog.getText(window, "Додати нотатку", "Ім'я нотатки: ")
+    if ok and note_name != "":
+        note = {
+            'name': note_name,
+            'content': '',
+            'tags': []
+        }
+        notes.append(note)
+        list_notes.addItem(note['name'])
+        print("Додано нотатку:", note)
+        save_notes_to_json()
+
+def save_note():
+    if list_notes.selectedItems():
+        key = list_notes.selectedItems()[0].text()
+        print(key)
+        for note in notes:
+            if note['name'] == key:
+                note['content'] = field_text.toPlainText()
+                save_notes_to_json()
+                print("Нотатка збережена:", note)
+                break
+    else:
+        print("Необрано нотатки для зберігання!")
+
+def del_note():
+    if list_notes.selectedItems():
+        key = list_notes.selectedItems()[0].text()
+        for note in notes:
+            if note['name'] == key:
+                print(note['name'] + ' ' + key)
+                notes.remove(note)
+                list_notes.takeItem(list_notes.row(list_notes.selectedItems()[0]))
+                print("Нотатка видалена:", note)
+                save_notes_to_json()
+                break
+    else:
+        print("Необрано нотатки для видалення!")
+
+def add_tag():
+    if list_notes.selectedItems():
+        key = list_notes.selectedItems()[0].text()
+        tag = field_tag.text()
+        for note in notes:
+            if note['name'] == key:
+                if tag not in note['tags']:
+                    note['tags'].append(tag)
+                    list_tags.addItem(tag)
+                    field_tag.clear()
+                    save_notes_to_json()
+                    print("Тег доданий до нотатки:", tag)
+                break
+    else:
+        print("Необрано нотатки для додавання тегу!")
 
 # Старт застосунку
 notes_win.show()
